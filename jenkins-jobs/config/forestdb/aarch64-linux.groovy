a3505da70852aa2b5e3de0e6582acd9c9ecb172a
Dave Rigby <daver@couchbase.com>
2021-08-13 16:21:45 +0100

    Revert "Run forestdb's tests sequentially"
    I has incorrectly assumed that forestdb's tests couldn't be run in
    parallel, due to some intermittent test failures I was observing.
    
    However this isn't the case (the failures were caused by actual test
    bugs) - tests can be run in parallel.
    
    This reverts commit ed572ea90ccf32189f8e8ea10f246a0fa3b8373f.
    
    Change-Id: Id98c684b3936b2d5d1b5a2c418dad304a29ce961
    Reviewed-on: http://review.couchbase.org/c/server-cv/+/159312
    Tested-by: Dave Rigby <daver@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- jenkins-jobs/config/forestdb/aarch64-linux.groovy
+++
@@ -1,6 +0,0 @@
-return {
-    // ForestDB's unit tests are not safe to run concurrently
-    // (they don't use unique filenames for example). Therefore must
-    // run tests sequentially.
-    TEST_PARALLELISM=1
-}
--- jenkins-jobs/config/forestdb/linux.groovy
+++ jenkins-jobs/config/forestdb/linux.groovy
@@ -1,8 +1,3 @@
 return {
-    // ForestDB's unit tests are not safe to run concurrently
-    // (they don't use unique filenames for example). Therefore must
-    // run tests sequentially.
-    TEST_PARALLELISM=1
-
     WARNING_THRESHOLD=45
 }
