55a35f2d93a8bdccf2242c9dfeece980613f7d25
Trond Norbye <trond.norbye@gmail.com>
2026-04-24 11:39:42 +0200

    Remove rockdb and breakpad from config
    We've removed those from master a long time ago
    
    Change-Id: I72e18f0d238201c17036acf8d28e9e5d6cbfb47c
    Reviewed-on: https://review.couchbase.org/c/server-cv/+/243979
    Reviewed-by: Jim Walker <jim@couchbase.com>
    Tested-by: Trond Norbye <trond.norbye@couchbase.com>

--- jenkins-jobs/config/kv_engine/threadsanitizer.groovy
+++ jenkins-jobs/config/kv_engine/threadsanitizer.groovy
@@ -2,14 +2,6 @@ return {
 CMAKE_ARGS="${CMAKE_ARGS} " +
            "-DBUILD_ENTERPRISE=1"
 
-// MB-25989: Disabling rocksdb tests while they are not stable.
 // Exclude breakpad tests as TSan doesn't like us crashing memcached...
-CTEST_ARGS="--exclude-regex ep_testsuite_basic.full_eviction.rocksdb|" +
-"ep_testsuite_basic.value_eviction.rocksdb|" +
-"ep_testsuite.value_eviction.rocksdb|" +
-"ep_testsuite.full_eviction.rocksdb|" +
-"ep_testsuite_xdcr.full_eviction.rocksdb|" +
-"ep_testsuite_xdcr.value_eviction.rocksdb|" +
-"ep_testsuite_dcp.value_eviction.rocksdb|" +
-"ep_testsuite_dcp.full_eviction.rocksdb|memcached-breakpad-test"
+CTEST_ARGS="--exclude-regex memcached-breakpad-test"
 }
